ae254affb4df83c845aed8f4cb4cc1a78845e31a
Muhammed Sahil AK <mdsahilak.dev@gmail.com>
2024-04-26 03:45:38 +0100

    add react route + async await apis

--- src/App.js
+++ src/App.js
@@ -1,35 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import './components/FoodItemRow.jsx'
-import FoodItemRow from './components/FoodItemRow.jsx';
+import React from 'react';
+import FoodItemsList from './components/FoodItemsList.jsx';
 
 function App() {
-  const [foodItems, setFoodItems] = useState([]);
-
-  useEffect(() => {
-    // Fetch data from your API
-    fetch('https://w2000528-api.azurewebsites.net/api/foodItems')
-      .then((response) => response.json())
-      .then((data) => setFoodItems(data))
-      .catch((error) => console.error('Error fetching data:', error));
-  }, []);
-
-  console.log(foodItems);
-
   return (
     <div className="App">
       <h1> The Cloud Kitchen </h1>
 
       <hr />
-      <p>Welcome to the Cloud Kitchen of the Future. Browse our menu below:</p>
+      <p>Welcome to the Cloud Kitchen of the Future.</p>
       <hr />
 
-      {
-        foodItems.map((item) => (
-          <FoodItemRow key={item.foodItemId} item={item} />
-        ))
-      }
+      <FoodItemsList />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/FoodItemsList.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import FoodItemRow from './FoodItemRow.jsx';
+import axios from "axios";
+
+const FoodItemsList = () => {
+    const [foodItems, setFoodItems] = useState([]);
+
+    useEffect(() => {
+        // Fetch data from your API
+        // fetch('https://w2000528-api.azurewebsites.net/api/foodItems')
+        //   .then((response) => response.json())
+        //   .then((data) => setFoodItems(data))
+        //   .catch((error) => console.error('Error fetching data:', error));
+
+        const fetchFoodItems = async () => {
+            try {
+                let respone = await axios.get('https://w2000528-api.azurewebsites.net/api/foodItems');
+                setFoodItems(respone.data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchFoodItems()
+    }, []);
+
+    console.log(foodItems);
+
+    return (
+        <div className="FoodItemsList">
+            {
+                foodItems.map((item) => (
+                    <FoodItemRow key={item.foodItemId} item={item} />
+                ))
+            }
+        </div>
+    );
+}
+
+export default FoodItemsList
